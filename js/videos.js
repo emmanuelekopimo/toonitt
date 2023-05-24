d605d3590b4676311aa971bfe7c41ab5d4beb3e3
Emmanuel Ekopimo <emmanuelekopimo@gmail.com>
2023-05-24 01:01:51 -0700

    Updates

--- js/videos.js
+++ js/videos.js
@@ -26,6 +26,19 @@ fetch(`https://outrageous-elk-wear.cyclic.app/?page=${page}`)
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowfullscreen></iframe>`;
+      if (index === 3) {
+        content += `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1186998729200892"
+              crossorigin="anonymous"></script>
+              <ins class="adsbygoogle"
+              style="display:block"
+              data-ad-format="fluid"
+              data-ad-layout-key="-ed+6k-30-ac+ty"
+              data-ad-client="ca-pub-1186998729200892"
+              data-ad-slot="9165255911"></ins>
+              <script>
+              (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>`;
+      }
       element.innerHTML = content;
       latest.append(element);
     });
